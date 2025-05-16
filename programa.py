ee64a46dbbbd42f677026903c780e2c809fa5dc2
Anny Carolline <annacaroline.8002@gmail.com>
2025-05-16 17:47:27 -0300

    Adicionando pesquisa de satisfação

--- programa.py
+++ programa.py
@@ -1,2 +1,6 @@
 import requests
 print("Meu programa é relevante para a história da humanidade!")
+
+nota = input("Qual sua nota pra relevância desse projeto?")
+
+print(f"Esse projeto tem uma relevância de {nota}")
